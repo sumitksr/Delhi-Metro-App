d5d8e0bf889a39fbf1ed1cda42a4fd42c80673bf
Sumit kumar <sumitksr6223@gmail.com>
2025-01-23 02:39:38 +0530

    Update metroApp.cpp

--- metroApp.cpp
+++ metroApp.cpp
@@ -19,7 +19,6 @@ string normalize(const string& str) {
     transform(result.begin(), result.end(), result.begin(), ::tolower);
     return result;
 }
-
 class MetroGraph {
 private:
     unordered_map<string, vector<pair<pair<string, int>, string>>> adjList;
